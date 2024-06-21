75b4c91f7896b97a59abf22f1c9c0f4933163b91
AlexKonkin92 <konkin.alex777@mail.ru>
2024-06-21 16:26:15 +0300

    rewrite main.py and update config file

--- config.py
+++ config.py
@@ -3,16 +3,14 @@ from dotenv import load_dotenv
 
 class Config():
     load_dotenv()
+    LOGIN = os.environ.get('LOGIN')
+    PASSWORD = os.environ.get('PASSWORD')
     DOMAIN = os.environ.get('DOMAIN')
-    AUTH_URL = f'https://{DOMAIN}/ipa/session/login_password'
-    JSON_RPC_URL = f'https://{DOMAIN}/ipa/json'
-    SENDER_POST = os.environ.get('SENDER_POST')
-    REFERER_URL = f'https://{DOMAIN}/ipa/ui/'
-    SMTP_PROVIDER = os.environ.get('SMTP_PROVIDER')
-    SMTP_PORT = os.environ.get('SMTP_PORT')
-    SMTP_PASSWORD = os.environ.get('SMTP_PASSWORD')
-    ADMIN_USER = os.environ.get('ADMIN_USER')
-    ADMIN_PASS = os.environ.get('ADMIN_PASS')
+    REFERER_URL = f'https://{DOMAIN}/'
+    AUTH_URL = f'https://{DOMAIN}/api/auth/login'
+    PROJECTS_URL = f'https://{DOMAIN}/api/projects/get-list'
+    VARIABLES_URL = f'https://{DOMAIN}/api/users/get-global-variables-by-priority'
+    CONTENT_TYPE = os.environ.get('CONTENT_TYPE')
     VERIFY_SSL = os.environ.get('VERIFY_SSL', 'true').lower() in {"true", "1"}
 
 config = Config()
--- main.py
+++ main.py
@@ -1,56 +1,64 @@
 import requests
+from config import Config
 
-def get_auth_session():
+
+def global_variables_view():
+    try:
+        session, auth_response = get_auth_session()
+        auth_token = auth_response['token']
+        projects_dict = get_projects_dict(session, auth_token)
+        get_variables_and_print(session, auth_token, projects_dict)
+    # except HTTPError as e:
+    #     print(f"HTTP error during authentication or user validation: {str(e)}", 400)
+    except KeyError as e:
+        print(f"Key error: {str(e)}", 400)
+    except Exception as e:
+        print(f"Unknown error: {str(e)}", 400)
+
+
+def get_auth_session() -> dict:
     session = requests.Session()
     data = {
-        "login": "admin",
-        "password": "Werq12345"
+        "login": Config.LOGIN,
+        "password": Config.PASSWORD
     }
-    headers = {'Content-Type': 'application/json'}
-    response = session.post('https://dev.ks.works/api/auth/login', json=data, headers=headers, verify=False)
+    headers = {'Content-Type': Config.CONTENT_TYPE,
+        'Referer': Config.REFERER_URL
+        }
+    response = session.post(Config.AUTH_URL, json=data, headers=headers, verify=Config.VERIFY_SSL)
     response.raise_for_status()
-    print("Статус код ответа аутентификации:", response.status_code)
-
     response_data = response.json()
     return session, response_data
 
-def get_variables_and_projects(session, auth_token):
-    # Получаем список проектов
-    projects_url = 'https://dev.ks.works/api/projects/get-list'
-    projects_headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {auth_token}'
+def get_projects_dict(session: requests.Session, auth_token: str) -> dict:
+    headers = {
+        'Content-Type': Config.CONTENT_TYPE,
+        'Authorization': f'Bearer {auth_token}',
+        'Referer': Config.REFERER_URL
     }
-    projects_data = {
+    data = {
         "limit": 0,
         "page": 0,
         "perPage": 0,
         "term": ""
     }
-    
-    projects_response = session.post(projects_url, headers=projects_headers, json=projects_data, verify=False)
-    projects_response.raise_for_status()
-    projects_list = projects_response.json()
+    response = session.post(Config.PROJECTS_URL, headers=headers, json=data, verify=Config.VERIFY_SSL)
+    response.raise_for_status()
+    projects_list = response.json()
     projects_dict = {project['uuid']: project for project in projects_list['data']}
-    
-    # Получаем глобальные переменные по приоритету
-    variables_url = 'https://dev.ks.works/api/users/get-global-variables-by-priority'
-    variables_headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {auth_token}'
+    return projects_dict
+
+def get_variables_and_print(session: requests.Session, auth_token: str, projects_dict: dict) -> str:
+    headers = {
+        'Content-Type': Config.CONTENT_TYPE,
+        'Authorization': f'Bearer {auth_token}',
+        'Referer': Config.REFERER_URL
     }
-    variables_response = session.post(variables_url, headers=variables_headers, verify=False)
-    variables_response.raise_for_status()
-    
-    variables = variables_response.json()
-    
+    response = session.post(Config.VARIABLES_URL, headers=headers, verify=Config.VERIFY_SSL)
+    response.raise_for_status()
+    variables = response.json()
     for var in variables['defaultVariables']:
-        print(f"{var['value']['name']} - {var['eventType']} - {var['projectUuid']} - {var['uuid']} - {projects_dict.get(var['projectUuid'], {}).get('name', 'Н/Д')} - {projects_dict.get(var['projectUuid'], {}).get('description', 'Н/Д') if projects_dict.get(var['projectUuid'], {}).get('description') else 'Н/Д'}")
+        print(f"{var['value']['name']} - {var['eventType']} - {var['projectUuid']} - {var['uuid']} - {projects_dict.get(var['projectUuid'], {}).get('name', 'Н/Д')} - {projects_dict.get(var['projectUuid'], {}).get('description', 'Н/Д')}")
 
-
-session, auth_response = get_auth_session()
-auth_token = auth_response['token']
-get_variables_and_projects(session, auth_token)
-
-
-#print(f"{var['value']['name']} - {var['eventType']} - {var['projectUuid']} - {var['uuid']} - {projects_dict.get(var['projectUuid'], {}).get('name', 'Н/Д')} - {projects_dict.get(var['projectUuid'], {}).get('description', 'Н/Д')}")
+if __name__ == "__main__":
+    global_variables_view()
